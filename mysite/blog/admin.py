7cee3a5b0346e3d736df1fa5a7bee51a96c5fb57
Munoto <zeron.masimov@mail.ru>
2024-03-01 17:34:45 +0500

    created and migrated models, created custom managers and etc

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status'] # атрибуты полей, которые надо вывести
+    list_filter = ['status', 'created', 'publish', 'author'] # фильтр, правая боковая панель
+    search_fields = ['title', 'body'] # поиск по имени поста и содержимого
+    prepopulated_fields = {'slug': ('title',)} #автозаполнение слага по тайтл
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish'] #сортировка по умолчанию
+
+
